20ac4dfaa1dadf74ef50c66f1c11ca4f85de606e
ming <rkdrornf99@naver.com>
2020-06-10 19:08:17 +0900

    주석 추가

---
+++ binary/binary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  binary
+//
+//  Created by minjung kang on 10/06/2020.
+//  Copyright © 2020 ming. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
