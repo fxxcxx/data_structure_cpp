14053eb3b0a13a3b9f874950e81124f1d256d2a9
Ming <63344889+fxxcxx@users.noreply.github.com>
2020-06-15 23:51:38 +0900

    add remark, checking

--- 10__객체_배열/Main.cpp
+++ 10__객체_배열/Main.cpp
@@ -7,8 +7,9 @@ class Circle
 	public:
 		int x,y;
 		int radius;
-		Circle() :x{0}, y{0}, radius{0}{ }
+		Circle() :x{0}, y{0}, radius{0}{ } //배열이라 대괄호 셈미콜론 없어
 		Circle(int x, int y, int r):x{x}, y{y}, radius{r}{ }
+	//r을 radius로 받는다...ㅁ
 	void print(){
 		cout<<"반지름:"<<radius<<"@("<<x<<y<<","<<y<<")"<<endl;
 }
@@ -16,10 +17,10 @@ class Circle
 
 int main(void)
 {
-	Circle objArray[10];
+	Circle objArray[10];//배열
 	
-	for(Circle& c: objArray){
-		c.x = rand()%500;
+	for(Circle& c: objArray){ //class circle 이용, objArray서 받아와
+		c.x = rand()%500;//랜덤함수자나 ㅡㅡ
 		c.y = rand()%300;
 		c.radius = rand()%100;
 	}
