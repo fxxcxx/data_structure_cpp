28423d0b4de71e4c506b8065a16c2a47d3099dd4
Ming <63344889+fxxcxx@users.noreply.github.com>
2020-06-16 02:28:36 +0900

    시험 백타 나온다

--- 26__단순연결리스트/Main.cpp
+++ 26__단순연결리스트/Main.cpp
@@ -10,25 +10,26 @@ class Node{
             m=a; //class public에 선언한 m에 넣는거, clas 기본과 똑같쥬?
             link=NULL;
         }
-    void insertNext(Node *nextNode){ //넥스트에 삽입
+    void insertNext(Node *nextNode){ // 내 뒤에 노드 추가
         nextNode->link=link;
         link=nextNode;
     }
     
     void insertFront(Node *frontNode){ //앞에 삽입
-        frontNode->link=link;
-        link=frontNode;
+        frontNode->link=link; //프론트의 링크를 링크로 바꿔줌(붙여)
+        link=frontNode; //링크를 프론트노드로
     }
     
     void removeNext(){ //넥스트 지움
         if(link!=NULL) link=link->link;
+        //앞의 링크를 링크의 링크(뛰어 넘어가)
     }
     
     void display(){
-        Node *p=link;
-        while(p!=NULL){
+        Node *p=link; //헤드의 링크를...
+        while(p!=NULL){ //n개
             cout<< p->m <<" ";
-            p=p->link;
+            p=p->link; //p 를 p의 링크로 바꾸래
         }
         cout<<endl;
     }
@@ -36,7 +37,7 @@ class Node{
 
 int main()
 {
-    
+    //앞의 예제 : 포인터로 했으나 여긴 head라는 객체를 만듦
     Node head; //head라는 노드 맹긂(맹그로브 아님 ㅎ)
     head.insertNext(new Node('a'));
     head.link->insertNext(new Node('b'));
