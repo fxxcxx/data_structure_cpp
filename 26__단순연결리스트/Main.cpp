7a37fa38903da18fb3905208a9ab0c6f53db8248
Ming <63344889+fxxcxx@users.noreply.github.com>
2020-06-16 02:17:31 +0900

    add remark. 킹리적 갓심인데 이거 잘 봐야될듯..

--- 26__단순연결리스트/Main.cpp
+++ 26__단순연결리스트/Main.cpp
@@ -2,57 +2,57 @@
 using namespace std;
 
 class Node{
-	public:
-		char m;
-		Node *link;
-	
-		Node(char a=0){
-			m=a;
-			link=NULL;
-		}
-	void insertNext(Node *nextNode){
-		nextNode->link=link;
-		link=nextNode;
-	}
-	
-	void insertFront(Node *frontNode){
-		frontNode->link=link;
-		link=frontNode;
-	}
-	
-	void removeNext(){
-		if(link!=NULL) link=link->link;
-	}
-	
-	void display(){
-		Node *p=link;
-		while(p!=NULL){
-			cout<<p->m<<" ";
-			p=p->link;
-		}
-		cout<<endl;
-	}
+    public:
+        char m;
+        Node *link; //node를 가르키는 ponter link
+    
+        Node(char a=0){
+            m=a; //class public에 선언한 m에 넣는거, clas 기본과 똑같쥬?
+            link=NULL;
+        }
+    void insertNext(Node *nextNode){ //넥스트에 삽입
+        nextNode->link=link;
+        link=nextNode;
+    }
+    
+    void insertFront(Node *frontNode){ //앞에 삽입
+        frontNode->link=link;
+        link=frontNode;
+    }
+    
+    void removeNext(){ //넥스트 지움
+        if(link!=NULL) link=link->link;
+    }
+    
+    void display(){
+        Node *p=link;
+        while(p!=NULL){
+            cout<< p->m <<" ";
+            p=p->link;
+        }
+        cout<<endl;
+    }
 };
 
 int main()
 {
-	
-	Node head;
-	head.insertNext(new Node('a'));
-	head.link->insertNext(new Node('b'));
-	head.link->link->insertNext(new Node('c'));
-	
-	head.insertFront(new Node('d'));
-	head.insertNext(new Node('e'));
-	head.insertFront(new Node('f'));
-	head.display();
-	
-	head.removeNext();
-	head.display();
-	
-	head.link->link->insertNext(new Node('g'));
-	head.link->link->insertFront(new Node('h'));
-	head.display();
-	
-	return 0;
+    
+    Node head; //head라는 노드 맹긂(맹그로브 아님 ㅎ)
+    head.insertNext(new Node('a'));
+    head.link->insertNext(new Node('b'));
+    head.link->link->insertNext(new Node('c')); //a링크 다음에 b 다음에 c
+    
+    head.insertFront(new Node('d'));
+    head.insertNext(new Node('e'));
+    head.insertFront(new Node('f'));
+    head.display(); //fedabc
+    
+    head.removeNext();
+    head.display(); //edabc
+    
+    head.link->link->insertNext(new Node('g'));
+    head.link->link->insertFront(new Node('h'));
+    head.display(); //edhgabc, g들가고 그 다음에 h또 들어감
+    
+    return 0;
 }
